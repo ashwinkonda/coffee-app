83a4a414a1a0c2983d40bb006e973305341a0c83
Ashwin Konda <ashwinkonda5@gmail.com>
2025-05-25 20:51:05 -0700

    Add exclamation point

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def menu():
         elif user_input == "4":
             prompt_find_best_method(connection)
         else:
-            print("Hello, world!")
+            print("Hello, world!!")
             print("Invalid input, please try again!")
 
 
